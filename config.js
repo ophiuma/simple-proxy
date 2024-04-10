7a765a75715a56c895e6516729655f8ae31fdc41
ophiuma <166657009+ophiuma@users.noreply.github.com>
2024-04-10 22:45:11 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://steady-praline-81f4a1.netlify.app",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTc0ZDhmZWQxOTFlZDg2ZGM0NmViYjljYjIxZjg2NyIsInN1YiI6IjY2MTZmNTAyNTU0NWNhMDE2MzI0MGM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LtdwH_bv4bbDA8a-rrJK3XfiwDQJYrdgO8p4A-XFvjM",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: null,
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: "",
+};
